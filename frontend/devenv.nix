8b2f27ab71e7ac569e0dbb6e605f3de2abced4c7
Ivan Petrov <ivanpetrov.main@gmail.com>
2026-03-13 06:35:42 -0600

    feat: add frontend devenv

---
+++ frontend/devenv.nix
@@ -0,0 +1,36 @@
+{ pkgs, config, lib, ... }: {
+
+  cachix.enable = false;
+
+  #shows nice TUI
+  process.manager.implementation = "process-compose";
+
+  # loads .env
+  dotenv.enable = true;
+
+  packages = [ 
+  ];
+  
+  languages.javascript = {
+    enable = true;
+    package = pkgs.nodejs_22;
+    npm.enable = true;
+    npm.install.enable = true; 
+  };
+
+  # processes
+  processes = {
+    frontend.exec = "npm run dev";
+  };
+
+
+  scripts.vue-setup.exec = ''
+    if [ ! -f "package.json" ]; then
+      npm create vue@latest
+    else
+      echo "Project exists. Ensuring dependencies are installed..."
+      npm install
+    fi
+  '';
+
+}
