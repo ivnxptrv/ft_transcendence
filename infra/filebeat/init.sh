e01dce2cd24baa8ea842a751d43010bdb442dc88
Ivan Petrov <ivanpetrov.main@gmail.com>
2026-03-20 05:56:04 -0600

    feat: add policy PUT request to filebeat init.sh

--- infra/filebeat/init.sh
+++ infra/filebeat/init.sh
@@ -6,7 +6,9 @@ KIBANA="${KIBANA:-kibana:5601}"
 
 echo "RUNNING!!$ELASTICSEARCH!!!!!!$KIBANA!!!!!!!!!!!!!!!!!!!!!!!!!!!";
 
-sleep 15;
+until curl -s http://$ELASTICSEARCH > /dev/null; do
+  sleep 5
+done
 
 filebeat setup -e --strict.perms=false \
   --index-management \
@@ -22,6 +24,10 @@ filebeat setup -e --strict.perms=false \
   -E output.logstash.enabled=false \
   -E "output.elasticsearch.hosts=[$ELASTICSEARCH]" \
   -E "setup.kibana.host=$KIBANA"
+
+curl -X PUT "http://$ELASTICSEARCH/_ilm/policy/filebeat" \
+     -H 'Content-Type: application/json' \
+     -d @/usr/share/filebeat/policy.json
 
 echo "------code: $?-----------------------"
 
