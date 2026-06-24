5d7547040d70b9430485e980e5b97089090f8484
Ivan Petrov <ivanpetrov.main@gmail.com>
2026-06-24 01:23:48 -0600

    feat: add init of working identity container

--- infra/postgres/README.md
+++ infra/postgres/README.md
@@ -2,7 +2,11 @@ docker exec -it postgres psql -U postgres -d postgres
 
 docker exec -it postgres bash
 
+# UNIX socket
 psql -U postgres -d postgres
+
+# TCP socket
+psql -h localhost -p 5432 -U postgres -d postgres 
 
 # Show all databases in postrges
 \l 
--- services/identity/entrypoint.sh
+++ services/identity/entrypoint.sh
@@ -5,10 +5,17 @@ set -e
 # # Read secrets from mounted files
 DB_PASS=$(cat /run/secrets/postgres_identity_pass)
 
-# # Build the full URL
-export DATABASE_URL="postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
+export WEB_URL="http://${WEB_HOST}:${WEB_PORT}";
+export IDENTITY_URL="http://${IDENTITY_HOST}:${IDENTITY_PORT}";
+export LEDGER_URL="http://${LEDGER_HOST}:${LEDGER_PORT}";
+export SEMANTIC_URL="http://${SEMANTIC_HOST}:${SEMANTIC_PORT}";
+export INTERACTION_URL="http://${INTERACTION_HOST}:${INTERACTION_PORT}";
+
+export DATABASE_URL="postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
 echo "$DATABASE_URL"
 
-npx prisma migrate deploy
+set -e
+alembic upgrade head
+
 echo "$@"
 exec "$@"
